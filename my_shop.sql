ca7eb3710e8803a4b3d35ed1dfed4b4336a3c23c
sacha <sacha.blin-zwertvaegher@epitech.eu>
2021-10-28 09:11:17 +0200

    updated bdd

--- my_shop.sql
+++ my_shop.sql
@@ -1,60 +1,157 @@
--- --------------------------------------------------------
--- Hôte :                        127.0.0.1
--- Version du serveur:           5.6.25-0ubuntu0.15.04.1 - (Ubuntu)
--- SE du serveur:                debian-linux-gnu
--- HeidiSQL Version:             9.3.0.4984
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 4.9.5deb2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3306
+-- Généré le : mer. 27 oct. 2021 à 16:56
+-- Version du serveur :  8.0.27-0ubuntu0.20.04.1
+-- Version de PHP : 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Export de la structure de la base pour my_shop
-CREATE DATABASE IF NOT EXISTS `my_shop` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `my_shop`;
+--
+-- Base de données : `my_shop`
+--
 
+-- --------------------------------------------------------
 
--- Export de la structure de table my_shop.categories
-CREATE TABLE IF NOT EXISTS `categories` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+--
+-- Structure de la table `categories`
+--
+DROP DATABASE IF EXISTS my_shop;
+CREATE DATABASE my_shop;
+
+USE my_shop;
+
+DROP TABLE IF EXISTS `categories`;
+
+CREATE TABLE `categories` (
+  `id` int NOT NULL,
   `name` varchar(255) DEFAULT NULL,
-  `parent_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `parent_id` int DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
--- Export de données de la table my_shop.categories : ~0 rows (environ)
-/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+-- --------------------------------------------------------
 
+--
+-- Structure de la table `products`
+--
 
--- Export de la structure de table my_shop.products
-CREATE TABLE IF NOT EXISTS `products` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL DEFAULT '0',
-  `price` int(11) NOT NULL DEFAULT '0',
-  `category_id` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `products`;
 
--- Export de données de la table my_shop.products : ~0 rows (environ)
-/*!40000 ALTER TABLE `products` DISABLE KEYS */;
-/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+CREATE TABLE `products` (
+  `id` int NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `price` int NOT NULL DEFAULT '0',
+  `categorie` text,
+  `size` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `color` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `sexe` text CHARACTER SET utf8 COLLATE utf8_general_ci
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+--
+-- Déchargement des données de la table `products`
+--
 
--- Export de la structure de table my_shop.users
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) DEFAULT NULL,
+INSERT INTO `products` (`id`, `name`, `price`, `categorie`, `size`, `color`, `sexe`) VALUES
+(18, 'Titre', 2, NULL, '0', 'bleu', NULL),
+(19, 'Titre', 6, NULL, '0', 'bleu', NULL),
+(20, 'Titre', 6, NULL, '0', 'bleu', NULL),
+(21, 'Titre', 11, NULL, '0', 'bleu', NULL),
+(22, 'Titre', 13, NULL, '0', 'blanc', NULL),
+(23, 'Titre', 13, NULL, '0', 'blanc', NULL),
+(24, 'Titre', 13, NULL, 'S', 'blanc', NULL),
+(25, 'Tee-shirt Clotaire ', 10, NULL, 'S', 'bleu', NULL),
+(26, 'Tee-shirt Clotaire soleil', 54, NULL, 'XL', 'noir', 'Homme'),
+(27, 'Tee-shirt Clotaire pluie', 123, 'manche longue', 'L', 'noir', 'Homme'),
+(28, 'Tee-shirt Sacha', 76, 'manche courte', 'M', 'blanc', 'Femme');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `firstname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `name` varchar(10) NOT NULL,
   `password` varchar(255) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
-  `admin` tinyint(4) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `postcode` varchar(5) DEFAULT NULL,
+  `ville` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `address` text CHARACTER SET utf8 COLLATE utf8_general_ci,
+  `phone` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `admin` tinyint DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
--- Export de données de la table my_shop.users : ~0 rows (environ)
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `name`, `password`, `email`, `postcode`, `ville`, `address`, `phone`, `admin`) VALUES
+(1, 'Jim', 'Titre', '0', 'arthur.noizet@gmail.com', '59000', NULL, NULL, '771109875', NULL),
+(2, 'Jim', 'Titre', 'arthur', 'arthur.noizet@gmail.com', '59000', NULL, '', '771109875', NULL),
+(3, 'Jim', 'Andeson', 'arthur', 'arthur.noizet@gmail.com', '59000', NULL, NULL, '771109875', NULL),
+(4, 'Jim', 'Andeson', 'arthur', 'arthur.noizet@gmail.com', '59000', NULL, NULL, '771109875', NULL),
+(5, 'Jim', 'Andeson', 'arthur', 'arthur.noizet@gmail.com', '59000', NULL, 'Place Rihour', '771109875', NULL);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
